436eaf1b63b897904b7403390b970f8dceed3502
Jack Jin <gjinjian@gmail.com>
2014-02-16 21:36:22 +0800

    元素类型定义

---
+++ element.h
@@ -0,0 +1,24 @@
+/**
+ * @file element.h
+ * @brief 本文件定义所有数据结构存储对象的类型
+ * @author Jack Jin - gjinjian@gmail.com
+ * @version v0.1
+ * @date 2014-01-19
+ */
+#ifndef _ELEMENT_H_
+#define _ELEMENT_H_
+
+typedef int     Ele;
+typedef int *   PEle;
+typedef unsigned int size_t;
+
+#if 0
+typedef struct teacher{
+    char    *name;
+    int     age;
+    double  salary;
+}Ele, *PEle;
+#endif
+
+
+#endif 
